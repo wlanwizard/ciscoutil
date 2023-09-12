5b0c9665aae1bfcc2d20c4da9cdaa7bf534787d4
Jon Hurd <jon.hurd@gmail.com>
2023-09-12 09:50:26 -0600

    v2 with date time output and fixed for loop

--- cmd2file.py
+++ cmd2file.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 import getpass
+import datetime
 
 # Read from a list of hostnames to connect to
 hosts = open('hosts.txt', 'r')
@@ -21,25 +22,35 @@ for host in hosts:
         'password': passWord
     }
 
-# Netmiko SSH Connection Handler
-net_connect = ConnectHandler(**devices)
+    # Netmiko SSH Connection Handler
+    net_connect = ConnectHandler(**devices)
 
-#open file to write command output
-file = open(host + '_output.txt', 'w')
+    #open file to write command output
+    # Get the current date and time
+    current_date = datetime.datetime.now()
+
+    # Convert the current date to a string
+    current_date_str = str(current_date)
+    current_date_str = current_date_str.replace(" ", "")
+    current_date_str = current_date_str.replace("/", "-")
+    current_date_str = current_date_str.replace(":", "-") 
+
+    #current_date = datetime.datetime.now().strftime("_%Y-%m-%d_%H:%M:%S")
+    file = open(host + "_" + current_date_str + '_output.txt', 'w')
 
 
-# Execute commands
-output = net_connect.send_command('skip-page-display')
-output = net_connect.send_command('show run aaa')
+    # Execute commands
+    output = net_connect.send_command('term len 0')
+    output = net_connect.send_command('show run aaa')
 
 
-# Print output to console screen
-print('————– Output from ' + host + '——————')
+    # Print output to console screen
+    print(host)
 
-print(output)
-print()
-print()
+    #print(output)
+    #print()
+    #print()
 
-# Write output to file above
-file.write(output)
-file.close()
+    # Write output to file above
+    file.write(output)
+    file.close()
